b155272330e2222308d23392270d53fa065cb369
danieldamian09 <humbertorivero9@gmail.com>
2022-04-20 21:21:23 -0300

    feat main container

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import {Container} from "react-bootstrap";
 
 function App() {
-
-  return (
-    <h1>Hola Mundo</h1>
-  )
+	return (
+		<>
+			<header className="py-5">
+				<h1>Buscador de Bebidas</h1>
+			</header>
+			<Container className="mt-5">1</Container>
+		</>
+	);
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+// import boostrap
+import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App'
 import './index.css'
 
