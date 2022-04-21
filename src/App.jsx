732adde48ebd420211de8449b63d8564a1398eb7
danieldamian09 <humbertorivero9@gmail.com>
2022-04-21 20:16:36 -0300

    add context  drinks categories

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import {Container} from "react-bootstrap";
 import Formulario from "./components/Formulario";
+import {CategoriasProvider} from "./context/CategriasProvider";
 
 function App() {
 	return (
-		<>
+		<CategoriasProvider>
 			<header className="py-5">
 				<h1>Buscador de Bebidas</h1>
 			</header>
 			<Container className="mt-5">
 				<Formulario />
 			</Container>
-		</>
+		</CategoriasProvider>
 	);
 }
 
