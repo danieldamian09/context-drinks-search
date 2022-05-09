5d0d3026f0ffc527ba0b00af37f5a7e12f2316e5
danieldamian09 <humbertorivero9@gmail.com>
2022-05-08 21:12:57 -0300

    Feat show drinks in the Bebidas component

--- src/components/Bebida.jsx
+++ src/components/Bebida.jsx
@@ -1,7 +1,21 @@
+import {Col, Card, Button} from 'react-bootstrap'
 
 const Bebida = ({bebida}) => {
   return (
-    <div>{ bebida.strDrink}</div>
+    <Col md={6} lg={4}>
+      <Card className='mb-4'>
+        <Card.Img variant="top" src={bebida.strDrinkThumb} alt={`Imagen de Bebida ${bebida.strDrink}`} />
+        <Card.Body>
+          <Card.Title>{bebida.strDrink}</Card.Title>
+          <Card.Text>
+            {bebida.strInstructions}
+          </Card.Text>
+          <Button className='w-100 text-uppercase mt-2' variant="warning">
+            Ver
+          </Button>
+        </Card.Body>
+      </Card>
+    </Col>
   )
 }
 
